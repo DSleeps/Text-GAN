3b63ddad87a0c84f9327879135ee0b7183e540ae
Dylan Sleeper <dsleeper@mit.edu>
2020-04-21 00:32:40 -0700

    Added generator

---
+++ GAN.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn
+import numpy as np
+
+class Generator(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers, output_size, head_count, device):
+        super(Generator, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.head_count = head_count
+        self.device = device
+
+        self.lstm = nn.LSTM(input_size + hidden_size * head_count, hidden_size, num_layers=num_layers, batch_first=True)
+        self.attention = nn.Sequential(nn.Linear(2 * hidden_size, hidden_size),
+                                       nn.ReLU(),
+                                       nn.Linear(hidden_size, head_count),
+                                       nn.ReLU())
+
+        self.output_model = nn.Sequential(nn.Linear((1 + head_count) * hidden_size, output_size),
+                                          nn.LogSoftmax())
+
+    def forward(self, inputs, max_output_len):
+        batch_size = inputs.shape[0]
+        outputs = torch.zeros(batch_size, max_output_len, self.output_size).to(self.device)
+
+        prev_hidden = torch.zeros(batch_size, self.num_layers, self.hidden_size)
+        prev_cell = torch.zeros(batch_size, self.num_layers, self.hidden_size)
+        hiddens = torch.zeros(batch_size, 1, self.hidden_size)
+        for i in range(max_output_len):
+            # Reformat the hidden layer to input into the attention model
+            last_hidden =  prev_hidden[:,-1,:]
+            dup_hidden = torch.zeros(i+1, batch_size, self.hidden_size).to(self.device)
+            dup_hidden[:] = last_hidden
+            dup_hidden = dup_hidden.permute(1,0,2)
+
+            # First calculate the attention
+            attention = self.attention(torch.cat((dup_hidden, hiddens), dim=2))
+            # attention = torch.reshape(attention, (batch_size, max_sequence_len))
+            attention = nn.Softmax(dim = 1)(attention)
+            
+            # Reshape them for the matrix multiply
+            attention = torch.reshape(attention, (batch_size, i+1, self.head_count, 1))
+            r_hiddens = torch.reshape(hiddens, (batch_size, i+1, 1, self.hidden_size))
+
+            c_vecs = torch.sum(attention * r_hiddens, dim=1)
+            c_vecs = torch.reshape(c_vecs, (batch_size, 1, self.hidden_size * self.head_count))
+            
+            # Now feed that into the model
+            output, outs = self.lstm(torch.cat((inputs[:,i:i+1,:], c_vecs), dim=2), (prev_hidden.permute(1,0,2), prev_cell.permute(1,0,2)))
+            
+            prev_hidden = outs[0].permute(1,0,2)
+            prev_cell = outs[1].permute(1,0,2)
+            hiddens = torch.cat((hiddens, output), dim=1) 
+
+            # Run the hidden layer through the output model
+            outputs[:,i] = self.output_model(torch.cat((output[:,0,:], c_vecs[:,0,:]), dim=1))
+
+        return outputs
--- Test.py
+++ Test.py
@@ -1,7 +1,8 @@
 import torch
 from AutoEncoder import EncoderDecoder
 from LoadDataset import load_data
-
+from GAN import Generator
+'''
 hidden_size = 10
 num_layers = 2
 output_size = 20
@@ -18,3 +19,18 @@ model = EncoderDecoder(embedding_tensor, hidden_size, num_layers, output_size)
 #sequences = torch.randn(batch_size, max_seq_len, input_size)
 print(data.shape)
 print(model(data, lengths, max_output_len))
+'''
+
+input_size = 30
+hidden_size = 50
+num_layers = 2
+output_size = 10
+head_count = 5
+batch_size = 4
+max_output_len = 30
+
+model = Generator(input_size, hidden_size, num_layers, output_size, head_count, 'cpu')
+
+inputs = torch.randn(batch_size, max_output_len, input_size)
+
+print(model(inputs, max_output_len))
