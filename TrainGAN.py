9d1037181b156de33f23381f223178c6a5b8a78f
Dylan Sleeper <Dill_Pill_007@yahoo.com>
2020-04-23 10:13:07 -0700

    Fixed training

--- TrainGAN.py
+++ TrainGAN.py
@@ -11,14 +11,16 @@ print(device)
 
 # Training_parameters
 g_times = 1
-d_times = 5
+d_times = 1
 batch_size = 32
-iteration_count = 10000
+iteration_count = 20000
 max_output_len = 30
 
 # First load the dataset
 data, lengths, vocab, embedding_tensor = load_data('wikitext-103/wiki.train.tokens', max_output_len)
 inv_vocab = {v: k for k, v in vocab.items()}
+
+print(data.shape[0])
 
 # Generator parameters
 g_input_size = 100
@@ -26,7 +28,7 @@ g_hidden_size = 512
 g_output_size = embedding_tensor.shape[0] # The number of words in the vocab
 g_num_layers = 3
 g_head_count = 5
-g_learning_rate = 0.005
+g_learning_rate = 0.0002
 
 generator = Generator(g_input_size, g_hidden_size, g_num_layers, g_output_size, g_head_count, device).to(device)
 g_loss_fn = nn.NLLLoss()
@@ -37,7 +39,7 @@ d_input_size = g_output_size
 d_hidden_size = 256
 d_num_layers = 3
 d_head_count = 5
-d_learning_rate = 0.0005
+d_learning_rate = 0.0002
 
 discriminator = Discriminator(d_input_size, d_hidden_size, d_num_layers, d_head_count, device).to(device)
 d_loss_fn = nn.NLLLoss()
@@ -75,7 +77,6 @@ def get_d_output(g_out, d_out):
     temp[eos_indices[:,0],eos_indices[:,1]] = eos_indices[:,1]
     
     first_eos_indices = torch.min(temp, dim=1)
-
     return d_out[torch.arange(0,batch_size),first_eos_indices.values]
 
 
@@ -91,7 +92,7 @@ for i in range(iteration_count):
         auto_outs = autoencoder(seqs, s_lengths, max_output_len)
         
         # Now generate the generator outputs
-        input_seed = torch.empty(int(batch_size/2), g_input_size).normal_(mean=0, std=1.0).to(device)
+        input_seed = torch.empty(int(batch_size/2), g_input_size).normal_(mean=0.5, std=1.0).to(device)
         inputs = torch.zeros(max_output_len, int(batch_size/2), g_input_size).to(device)
         inputs[:] = input_seed
         g_outs = generator(inputs.permute(1,0,2), max_output_len)
@@ -100,7 +101,7 @@ for i in range(iteration_count):
         d_desired = torch.zeros(batch_size,dtype=torch.int64).to(device)
         d_desired[:int(batch_size/2)] = 0   # The first index is real
         d_desired[int(batch_size/2):] = 1   # The second index is fake inputs
-
+        
         d_outs = discriminator(d_inputs, max_output_len)
         d_loss = d_loss_fn(get_d_output(d_inputs, d_outs), d_desired)
         # d_loss = d_loss_fn(d_outs[:,-1], d_desired)
@@ -112,7 +113,7 @@ for i in range(iteration_count):
     out_example = None
     for g in range(g_times):
         # Generator outputs
-        input_seed = torch.empty(batch_size, g_input_size).normal_(mean=0, std=1.0).to(device)
+        input_seed = torch.empty(batch_size, g_input_size).normal_(mean=0.5, std=1.0).to(device)
         inputs = torch.zeros(max_output_len, batch_size, g_input_size).to(device)
         inputs[:] = input_seed
         
@@ -131,10 +132,11 @@ for i in range(iteration_count):
         g_loss.backward()
         g_optim.step()
 
-    if (i % 200 == 0):
+    if (i % 1000 == 0):
         # Calculate the time remaining
         secs_per_iter = (time.time() - start_time)/(i + 1)
         remaining_mins = int((secs_per_iter/60.) * (iteration_count - i))
         print('Iteration ' + str(i) + ' | Time Remaining: ' + str(remaining_mins) + ' mins')
         print(g_loss)
+        print(d_loss)
         print(make_sentence(out_example))
